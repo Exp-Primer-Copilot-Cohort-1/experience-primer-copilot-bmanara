c830d5c3684ce9ade78389ee960ddc96ab079cab
Brian Ma <65152569+bmanara@users.noreply.github.com>
2024-05-06 08:34:35 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,20 @@
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+app.use(bodyParser.json());
+
+// Read data from comments.json
+app.get('/comments', (req, res) => {
+  fs.readFile('comments.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Something went wrong!');
+      return;
+    }
+    res.send(data);
+  });
+});
+
